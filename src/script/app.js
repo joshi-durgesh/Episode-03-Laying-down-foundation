7248556b4f8eb5edc9ad82ca6b4364dcbb90383f
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-27 14:39:34 +0530

    creating element using jsx

--- src/script/app.js
+++ src/script/app.js
@@ -9,8 +9,26 @@ const contentReact = React.createElement(
   "This heading is created with React createElement 🚀"
 );
 
+//How we create element using jsx
+const contentJsx = (
+  <h1 className='heading'>This heading is created with jsx 💥</h1>
+);
+
 //creating root to render our element
 const root = ReactDOM.createRoot(document.getElementById("container"));
 
-//rendering our element to root
-root.render(contentReact);
+/**
+ * rendering our element to root
+ *
+ * How we render jsx element
+ *   -Rendering is same as rendering react element
+ *
+ * How rendering same like react element
+ *   -jsx is converted to react element with the help of parcel
+ *
+ * Does parcel do it by itsel
+ *    -No parcel manages the jsx element, it give the transpilation task to bable it is another module that parcel required to compile/transpil our jsx to react elment
+ */
+
+// root.render(contentReact);
+root.render(contentJsx);
