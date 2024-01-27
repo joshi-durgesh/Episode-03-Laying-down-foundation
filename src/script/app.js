320995a10d6a9ed61e7019618cea79e9dad411d0
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-27 14:25:43 +0530

    creating react element using createElement

--- src/script/app.js
+++ src/script/app.js
@@ -1,3 +1,16 @@
 //importing react in js
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+//How we create element using react
+const contentReact = React.createElement(
+  "h1",
+  { className: "heading" },
+  "This heading is created with React createElement 🚀"
+);
+
+//creating root to render our element
+const root = ReactDOM.createRoot(document.getElementById("container"));
+
+//rendering our element to root
+root.render(contentReact);
